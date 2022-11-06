ed70db4e3287e734feb0c8e70a40a09a4697f9ca
Omda <Omda18009@gmail.com>
2022-11-06 06:55:25 -0800

    mohmed a000ssungment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path : '' , component : LoginComponent},
+  {path : 'Login' , component : LoginComponent},
   {path : 'login' , component : LoginComponent},
   {path : 'create-employee' , component : CreateEmployeeComponent},
   {path : 'employees' , component : EmployeesComponent},
